292fcc14cb0b33923526fc82f76fc3dfac36f456
minhtai0611 <minhtainth06111999@gmail.com>
2024-09-11 20:18:10 +0700

    setup-express-server

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,24 @@
-function sum(a: number, b: number) {
-    return a + b;
+import express, {Application, Request, Response} from 'express';
+
+const app: Application = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Define a route
+app.get('/', async (req: Request, res: Response): Promise<Response> => {
+    return res.status(200).send({"message":"Hello World"});
+});
+
+// Start the server
+try {
+app.listen(PORT, (): void => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
 }
-console.log(sum(2, 6));
+catch (err: unknown) {
+    console.error(`Error: ${err}`);
+}
+
